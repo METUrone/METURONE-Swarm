43339b655a36de3538e4c082ac508fdd468b27db
alpylmz <ylmz.alp.e@gmail.com>
2021-08-20 14:41:43 +0300

    temizlik alp

--- GUI v3/formations.py
+++
@@ -1,9 +0,0 @@
-class Formations:
-	def __init__(self):
-		self.formations = {"Üçgen" : 3, "Kare" : 4 , "Beşgen " : 5 , "Altıgen" : 6 , "Yedigen" : 7 , "Sekizgen" : 8, "Dokuzgen" : 9 , "Ongen" : 10}
-		
-	
-	def AddFormation(self , name , dronePoses) :
-		self.formations[name] = dronePoses
-formations = Formations()
-
